d2cd08739db5f68701186cbdc64d7e96a216ecfa
1KA11e1 <103613210+1KA11e1@users.noreply.github.com>
2022-04-14 21:55:16 -0300

    Add files via upload

---
+++ classes/Funcionario.php
@@ -0,0 +1,13 @@
+<?php
+
+class Funcionario
+{
+    public  $nome;
+    public  $cpf;
+    public  $data_nascimento;
+    public  $endereco;
+    public  $telefone;
+    public  $email;
+  
+}
+?>
---
+++ classes/Servico.php
@@ -0,0 +1,11 @@
+<?php
+
+class Servico
+{
+    public  $nome;
+    public  $categoria;
+    public  $periodo;
+    public  $preço;
+
+}
+?>

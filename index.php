aaa1297716c1fd0581dbf184383fa0ea013b817c
1KA11e1 <103613210+1KA11e1@users.noreply.github.com>
2022-04-14 22:04:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,27 +12,20 @@ $funcionario -> cpf             = "988.877.677-86";
 $funcionario -> data_nascimento = "19/11/1599";
 $funcionario -> email           = "alekfumaça1599@gmail.com";
 
-$k8al = new Tenis();
-$k8al -> nome    = "Kyrie Infinity EP 8 Irving Aluminum.";
-$k8al -> ID_nike = "DC9134-400";
-$k8al -> tamanho = "41";
-$k8al -> cor     = "ALUMINUM/SAIL-BLACK";
-$k8al -> estoque = "298";
-$k8al -> preco   = "949.99"; 
-
-$regatanets = new Regatas();
-$regatanets -> nome    = "Brooklyn Nets swingman jersey classic edition 2021.";
-$regatanets -> nike_id = "GE480202J5";
-$regatanets -> tamanho = "M";
-$regatanets -> cor     = "Azul, branco e vermelho";
-$regatanets -> estoque = "38 restantes";
-$regatanets -> preco   = "487";
-
 $servico = new Servico();
 $servico -> nome      = "Leve ao cliente";
 $servico -> categoria = "Delivery";
 $servico -> periodo   = "10min a 7 dias dependendo do local destinado";
 $servico -> preco     = "De 10 a 40 reais";
+
+$produto = new ();
+$produto->setNome(“TenisERegatas 01”);
+$produto->setId();
+$produto->setTamanho();
+$produto->setCor();
+$produto->setEstoque();
+$produto->setPreco(9.99);
+$produto->adicionar();
 
 
 /*echo "<pre>";
